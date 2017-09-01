7ac51b3d549088355bc4eb0a673a834075f40f37
Abhinav Swami <abhinav.swami21@gmail.com>
2017-08-31 22:18:04 -0700

    Add search component class

---
+++ app/components/search.component.js
@@ -0,0 +1,17 @@
+// Import React
+import React from 'react';
+
+// Create search component class
+class Search extends React.Component {
+    render() {
+        return (
+            <div className="search">
+
+            </div>
+        );
+    }
+}
+
+// Export Search
+
+export default Search
