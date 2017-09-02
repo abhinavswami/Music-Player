ebada4ae5872518d03d42778983e83c3ced389fb
Abhinav Swami <abhinav.swami21@gmail.com>
2017-09-02 13:24:24 -0700

    Add Details component

--- app/app.js
+++ app/app.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Search from './components/search.component';
+import Details from './components/details.component';
 
 // Search component created as a class
 class App extends React.Component {
-    // render method is the most important
-    // render method returns JSX template
+    // render method is the most important render method returns JSX template
     render() {
         return (
-            <Search />
+            <div>
+                <Search/>
+                <Details title={'Title Track'}/> 
+            </div>
         );
     }
 }
 
 // Render to the ID content in the DOM
-ReactDOM.render(<App />, document.getElementById('content'));
+ReactDOM.render(
+    <App/>, document.getElementById('content'));
