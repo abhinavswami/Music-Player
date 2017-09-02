db338290c7a107be2e57e403e6ee7238f82c9e3f
Abhinav Swami <abhinav.swami21@gmail.com>
2017-09-02 11:34:18 -0700

    Add Search text box with autocomplete

--- app/components/search.component.js
+++ app/components/search.component.js
@@ -1,11 +1,24 @@
 // Import React
 import React from 'react';
+// Import React's Autocomplete component
+import Autocomplete from 'react-autocomplete';
 
 // Create search component class
 class Search extends React.Component {
     render() {
         return (
             <div className="search">
+                <Autocomplete
+                    ref="autocomplete"
+                    inputProps={{
+                    title: "Title"
+                }}
+                    value={this.props.autoCompleteValue}
+                    items={this.props.tracks}
+                    getItemValue={(item) => item.title}
+                    onSelect={this.props.handleSelect}
+                    onChange={this.props.handleChange}
+                    />
 
             </div>
         );
