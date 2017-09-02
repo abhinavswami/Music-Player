2258ddadcb43d167b6b30363bfef4a029793d6c0
Abhinav Swami <abhinav.swami21@gmail.com>
2017-09-02 11:33:23 -0700

    Add Search component

--- app/app.js
+++ app/app.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Search from './components/search.component';
+
 // Search component created as a class
-class Search extends React.Component {
+class App extends React.Component {
     // render method is the most important
     // render method returns JSX template
     render() {
         return (
-            <form>
-                <input type="text"/>
-                <input type="submit"/>
-            </form>
+            <Search />
         );
     }
 }
 
 // Render to the ID content in the DOM
-ReactDOM.render(<Search/>, document.getElementById('content'));
+ReactDOM.render(<App />, document.getElementById('content'));
