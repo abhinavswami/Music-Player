8d8b7bfee71bd95e51cf19edb00202aa549db406
Abhinav Swami <abhinav.swami21@gmail.com>
2017-09-02 12:43:44 -0700

    Add line styling

--- app/components/search.component.js
+++ app/components/search.component.js
@@ -5,6 +5,32 @@ import Autocomplete from 'react-autocomplete';
 
 // Create search component class
 class Search extends React.Component {
+    handleRenderItem(item, isHighlighted) {
+        // Some basic styles
+        const lineStyles = {
+            item: {
+                padding: '2px 6px',
+                cursor: 'default'
+            },
+
+            highlightedItem: {
+                color: 'white',
+                background: '#F38B72',
+                padding: '2px 6px',
+                cursor: 'default'
+            }
+        };
+
+        // Render list items
+        return (
+            <div
+            style={isHighlighted ? lineStyles.highlightedItem : lineStyles.item}
+            key = {item.id}
+            id={item.id}
+            >{item.title} </div>
+        )
+    }
+
     render() {
         return (
             <div className="search">
@@ -18,7 +44,7 @@ class Search extends React.Component {
                     getItemValue={(item) => item.title}
                     onSelect={this.props.handleSelect}
                     onChange={this.props.handleChange}
-                    />
+                    renderItem={this.handleRenderItem.bind(this)}/>
 
             </div>
         );
