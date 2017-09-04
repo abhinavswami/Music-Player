1d731b168bff84607f9dec6ba676ed58a83b278f
Abhinav Swami <abhinav.swami21@gmail.com>
2017-09-03 23:07:07 -0700

    Add AppContainer and remove child components

--- app/app.js
+++ app/app.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Search from './components/search.component';
-import Details from './components/details.component';
-import Player from './components/player.component';
-import Progress from './components/progress.component';
-import Footer from './components/footer.component';
+import AppContainer from './containers/app.container';
 
 // Search component created as a class
 class App extends React.Component {
     // render method is the most important render method returns JSX template
     render() {
-        return (
-            <div>
-                <Search />
-                <Details title={'Title Track'}/>
-                <Player/>
-                <Progress
-                position = {'0.5'}
-                elapsed={'00:00'}
-                total={'0:40'}/>
-                <Footer />
-            </div>
-        );
+        return (<AppContainer/>);
     }
 }
 
